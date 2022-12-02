032970d53a5073a44d5c5f175cfbb42d527e352b
Arthur Perton <arthur@webenapp.nl>
2022-12-02 11:40:36 +0100

    Delete unused js script

--- resources/js/popular.js
+++
@@ -1,8 +0,0 @@
-(function () {
-    var formData = new FormData();
-
-    formData.append('_token', csrfToken());
-    formData.append('entry', window.location.href);
-
-    navigator.sendBeacon('/!/popular/pageviews', formData);
-})();
