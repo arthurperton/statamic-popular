73cfeed1fbca7b49a7df7d826b86f6df7cf6295b
Arthur Perton <arthur@webenapp.nl>
2022-12-05 16:45:49 +0100

    Handle corrupt database

--- src/Pageviews/Database.php
+++ src/Pageviews/Database.php
@@ -3,6 +3,7 @@
 namespace ArthurPerton\Popular\Pageviews;
 
 use Illuminate\Database\ConnectionInterface;
+use Illuminate\Database\QueryException;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
@@ -11,11 +12,8 @@ use Statamic\Facades\Path;
 
 class Database
 {
-    // TODO handle errors (e.g. create database again)
-
     protected $path;
     protected $connection;
-    // protected $db;
 
     public function __construct()
     {
@@ -27,8 +25,6 @@ class Database
         ]]);
 
         $this->connection = 'popular';
-
-        // $this->create();
     }
 
     public function path(): string
@@ -47,21 +43,18 @@ class Database
             return;
         }
 
-        // if (!File::exists($this->path)) {
+        $this->db()->disconnect();
+
         File::put($this->path, '');
-        // }
 
         $schema = Schema::connection($this->connection);
 
-        // if (!$schema->hasTable('pageviews')) {
         $schema->create('pageviews', function (Blueprint $table) {
             $table->string('entry')->index();
             $table->timestamp('timestamp')->index();
         });
-        // }
 
-        $db = DB::connection($this->connection);
-        $db->statement('PRAGMA journal_mode=WAL;');
+        $this->db()->statement('PRAGMA journal_mode=WAL;');
     }
 
     public function delete()
@@ -71,12 +64,18 @@ class Database
 
     public function addPageview($entry, $timestamp = null)
     {
-        $this->db()->insert('INSERT INTO pageviews (entry, timestamp) VALUES (?, ?)', [$entry, $timestamp ?? time()]);
+        $timestamp = $timestamp ?? time();
+
+        $this->query(function ($db) use ($entry, $timestamp) {
+            $db->insert('INSERT INTO pageviews (entry, timestamp) VALUES (?, ?)', [$entry, $timestamp]);
+        });
     }
 
     public function getGroupedPageviews()
     {
-        $result = $this->db()->select('SELECT rowid FROM pageviews ORDER BY rowid DESC LIMIT 1');
+        $result = $this->query(function ($db) {
+            $db->select('SELECT rowid FROM pageviews ORDER BY rowid DESC LIMIT 1');
+        });
 
         if (! $result) {
             return null;
@@ -84,18 +83,37 @@ class Database
 
         $lastId = (string) $result[0]->rowid;
 
-        $results = $this->db()->select('SELECT entry, COUNT(*) AS views FROM pageviews WHERE rowid <= ? GROUP BY entry', [$lastId]);
+        $results = $this->query(function ($db) {
+            $db->select('SELECT entry, COUNT(*) AS views FROM pageviews WHERE rowid <= ? GROUP BY entry', [$lastId]);
+        });
 
         return [$results, $lastId];
     }
 
     public function deletePageViews($lastId)
     {
-        $this->db()->delete('DELETE FROM pageviews WHERE rowid <= ?', [$lastId]);
+        $this->query(function ($db) use ($lastId) {
+            $db->delete('DELETE FROM pageviews WHERE rowid <= ?', [$lastId]);
+        });
+    }
+
+    public function query(callable $callback, $retry = true)
+    {
+        try {
+            return $callback($this->db());
+        } catch (QueryException $e) {
+            \Log::error("The temporary pageviews database for the Popular addon seems to be corrupt and will be re-created now. Original exception: $e");
+            
+            $this->create(true);
+            
+            if ($retry) {
+                return $this->query($callback, false);
+            }
+        }
     }
 
     protected function db(): ConnectionInterface
     {
-        return DB::connection($this->connection); // TODO cache?
+        return DB::connection($this->connection);
     }
 }
