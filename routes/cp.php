530e332ebd30491303b628bd5be5dbfddae5665d
Arthur Perton <arthur@webenapp.nl>
2023-08-08 14:47:49 +0200

    Add support for Statamic v4

--- routes/cp.php
+++ routes/cp.php
@@ -1,5 +1,6 @@
 <?php
 
+use ArthurPerton\Popular\Http\Controllers\PageviewController;
 use Illuminate\Support\Facades\Route;
 
-Route::patch('/popular/pageviews/{id}', 'PageviewController@update');
+Route::patch('/popular/pageviews/{id}', [PageviewController::class, 'update']);
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use ArthurPerton\Popular\Http\Controllers\PageviewController;
 use Illuminate\Support\Facades\Route;
 
-Route::post('/!/popular/pageviews', 'PageviewController@store');
+Route::post('/!/popular/pageviews', [PageviewController::class, 'store']);
